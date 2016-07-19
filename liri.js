73943d0d6dd93930e5743d8c48a476e7603db2d2
Julian Biebelberg <jbiebelberg@gmail.com>
2016-07-19 10:25:55 -0400

    fixed twitter json issue and added other api functionality

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 var keys = require('./keys.js')
-//console.log(keys);
-
 var Twitter = require('twitter');
+var omdb = require('omdb')
+var spotify = require('spotify');
  
 var client = new Twitter({
   consumer_key: keys.twitterKeys.consumer_key,
@@ -10,14 +10,64 @@ var client = new Twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+var params = { screen_name: 'JulianBibble' };
+var command = process.argv[2];
+var argument = process.argv[3];
 
 
-//console.log(client);
+if (command == 'my-tweets') {
+    console.log('here')
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            }
 
-client.get('statuses/user_timeline', function(error, tweets, response){
-  if(error) throw error;
-  tweets = JSON.stringify(tweets, null, 4);
-  response = JSON.stringify(response, null, 4)
-  console.log(tweets);  // The favorites. 
-  console.log(response);  // Raw response object. 
-});
+        }
+    });
+}
+
+
+if (command == 'movie-this') {
+    console.log('movie');
+    omdb.get({ title: argument }, true, function(err, movie) {
+        if (err) {
+            return console.error(err);
+        }
+
+        if (!movie) {
+            return console.log('Movie not found!');
+        }
+
+        console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+        console.log(movie.plot);
+    });
+
+}
+
+
+if (command == 'spotify-this-song') {
+    console.log('spotify');
+    spotify.search({ type: 'track', query: argument}, function(err, data) {
+        
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+       
+        for (var i = 0; i<data.tracks.items.length; i++) {
+
+          console.log('song name: ' + data.tracks.items[i].name);
+          console.log('preview url:'+ data.tracks.items[i].preview_url);
+          console.log('album name: ' + data.tracks.items[i].album.name);
+          
+          for( var j = 0; j<data.tracks.items[i].artists.length; j++){
+
+             console.log('artist name: ' + data.tracks.items[i].artists[j].name);
+           }
+
+          }
+        
+       
+    })
+};
